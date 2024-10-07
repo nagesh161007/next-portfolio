03c0cd6ed0bd0573bc7f1bf358adc14141b3a846
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2024-10-07 01:13:37 -0400

    initial

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -10,10 +10,7 @@ const BlogPage = () => {
       <div className="text-center mb-10 mt-10">
         <FiBookOpen className="text-orange-600 mx-auto mb-10" size={40} />
         <h1 className="text-3xl font-bold">Blogs Written</h1>
-        <p className="text-gray-500 mt-8">
-          In my past experience, I have had the opportunity to work on diverse
-          projects and collaborate with renowned brands.
-        </p>
+        <p className="text-gray-500 mt-8"></p>
       </div>
       <div className="p-8 bg-white max-w-6xl mx-auto mt-10">
         {/* Icon and Heading Section */}
