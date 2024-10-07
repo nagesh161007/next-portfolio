ff93e99ab624ca6404473526e83957b802efc4e2
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2024-10-07 01:17:46 -0400

    initial

--- src/app/custom/Footer.tsx
+++ src/app/custom/Footer.tsx
@@ -5,7 +5,7 @@ import { profileConfig } from "@/app/config/profile";
 const Footer = () => {
   return (
     <footer className="bg-white py-4 mt-10 border-t">
-      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-end items-center">
         {/* Social Icons */}
         <div className="flex space-x-4 mb-4 md:mb-0">
           <a
@@ -29,11 +29,6 @@ const Footer = () => {
           >
             <FaTwitter className="text-gray-600 hover:text-black" size={24} />
           </a>
-        </div>
-
-        {/* Footer Text */}
-        <div className="text-gray-600">
-          <span className="text-orange-600">{profileConfig.name}</span>
         </div>
       </div>
     </footer>
